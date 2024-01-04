b6a52190a7ee7d8763f04e0c896d7788c0b26049
bb6363 <bb6363@g.rit.edu>
2024-01-04 12:57:41 -0500

    The simple training/testing is done

--- prediction.py
+++ prediction.py
@@ -2,19 +2,21 @@
 # @ Language: Python
 # @ File: prediction.py
 
-
 import pandas as pd
 import xgboost as xgb
 import matplotlib.pyplot as plt
 
-
 # Load the inflation dataset
 bitcoin_data = pd.read_csv('bitcoin.csv')
-bitcoin = bitcoin_data.dropna(subset=['open'])
+bitcoin = bitcoin_data.dropna(subset=['open', 'Volume_USD', 'Volume_BTC'])
+
+# Assuming df is your DataFrame
+bitcoin['Date'] = pd.to_datetime(bitcoin['Date'])
+bitcoin = bitcoin.sort_values(by='Date')
 
 # Splitting data into training and testing data sets (80-20%)
 train_bitcoin = bitcoin.iloc[:int(.80*len(bitcoin)), :]
-test_bitcoin = bitcoin.iloc[int(.20*len(bitcoin)):, :] 
+test_bitcoin = bitcoin.iloc[int(.80*len(bitcoin)):, :] 
 
 # Defining the features and target variable
 features = ['open', 'Volume_USD', 'Volume_BTC']
