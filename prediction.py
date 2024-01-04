22759e406ebaa11b940089fc76658b4f3f7b9c2b
bb6363 <bb6363@g.rit.edu>
2024-01-04 15:25:03 -0500

    Using yahoo finance API instead of dataset for better results

--- prediction.py
+++ prediction.py
@@ -2,25 +2,31 @@
 # @ Language: Python
 # @ File: prediction.py
 
+
 import pandas as pd
 import xgboost as xgb
 import matplotlib.pyplot as plt
+import yfinance as yf
 
-# Load the inflation dataset
-bitcoin_data = pd.read_csv('bitcoin.csv')
-bitcoin = bitcoin_data.dropna(subset=['open', 'Volume_USD', 'Volume_BTC'])
 
-# Assuming df is your DataFrame
-bitcoin['Date'] = pd.to_datetime(bitcoin['Date'])
-bitcoin = bitcoin.sort_values(by='Date')
+# Function to get historical Bitcoin data from Yahoo Finance
+def get_bitcoin_data():
+    bitcoin_data = yf.download('BTC-USD', start='2020-01-01', end='2023-01-01')
+    return bitcoin_data
+
+# Fetch Bitcoin data
+bitcoin_data = get_bitcoin_data()
+
+# Drop rows with missing 'open' values
+bitcoin = bitcoin_data.dropna(subset=['Open'])
 
 # Splitting data into training and testing data sets (80-20%)
 train_bitcoin = bitcoin.iloc[:int(.80*len(bitcoin)), :]
-test_bitcoin = bitcoin.iloc[int(.80*len(bitcoin)):, :] 
+test_bitcoin = bitcoin.iloc[int(.20*len(bitcoin)):, :] 
 
 # Defining the features and target variable
-features = ['open', 'Volume_USD', 'Volume_BTC']
-target = 'close'
+features = ['Open', 'Volume']
+target = 'Close'
 
 # Create and train model
 model = xgb.XGBRegressor()
@@ -32,7 +38,7 @@ value_predictions = model.predict(test_bitcoin[features])
 accuracy_value = model.score(test_bitcoin[features], test_bitcoin[target])
 print(accuracy_value)
 
-plt.plot(bitcoin['close'], label = 'Bitcoin Price')
-plt.plot(test_bitcoin['close'].index, value_predictions, label  = "Predictions")
+plt.plot(bitcoin['Close'], label = 'Bitcoin Price')
+plt.plot(test_bitcoin['Close'].index, value_predictions, label  = "Predictions")
 plt.legend()
 plt.show()
